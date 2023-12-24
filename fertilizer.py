56db8a83dea20b8710458a7d7cf0622fda59ddee
quocdatestweb <147794210+quocdatestweb@users.noreply.github.com>
2023-12-24 22:55:48 +0700

    up code v1
    up code v1

---
+++ fertilizer.py
@@ -0,0 +1,93 @@
+fertilizer_dic = {
+      'NHigh': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị N của đất cao và có thể làm phát triển cỏ dại.</h1></div>
+         <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/> Hãy xem xét những gợi ý sau:
+
+         <br/><br/> 1. <i> Phân bón </i> – thêm phân chuồng là một trong những cách đơn giản nhất để cải tạo đất bằng nitơ. Hãy cẩn thận vì có nhiều loại phân có hàm lượng nitơ khác nhau.
+
+         <br/> 2. <i>Cà phê xay </i> - tận dụng cơn nghiện buổi sáng để nuôi dưỡng thói quen làm vườn của bạn! Cà phê xay được coi là nguyên liệu làm phân xanh rất giàu nitơ. Một khi đất bị phân hủy, đất của bạn sẽ được cung cấp nitơ thơm ngon, hấp dẫn. Một lợi ích bổ sung của việc thêm bã cà phê vào đất của bạn là trong khi nó sẽ được ủ phân, nó cũng sẽ giúp tăng khả năng thoát nước cho đất của bạn.
+
+         <br/>3. <i>Trồng cây cố định đạm</i> – trồng các loại rau thuộc họ Fabaceae như đậu Hà Lan, đậu và đậu nành có khả năng tăng lượng nitơ trong đất của bạn
+
+         <br/>4. Trồng cây 'phân xanh' như bắp cải, ngô và bông cải xanh
+
+         <br/>5. <i>Sử dụng lớp phủ (cỏ ướt) khi trồng trọt</i> - Lớp phủ cũng có thể bao gồm mùn cưa và phế liệu gỗ mềm</div>""",
+
+      'Nlow': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị N trong đất của bạn thấp.</h1></div>
+         <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/> Hãy xem xét những gợi ý sau:
+         <br/><br/> 1. <i>Thêm mùn cưa hoặc dăm gỗ mịn vào đất của bạn</i> – carbon trong mùn cưa/măm gỗ ưa nitơ và sẽ giúp hấp thụ, hấp thụ lượng nitơ dư thừa.
+
+         <br/>2. <i>Trồng các loại cây giàu nitơ</i> – cà chua, ngô, bông cải xanh, bắp cải và rau bina là những ví dụ về các loại cây phát triển mạnh nhờ nitơ và sẽ hút khô nitơ.
+
+         <br/>3. <i>Nước</i> – ngâm đất bằng nước sẽ giúp thấm nitơ sâu hơn vào đất, khiến cây trồng của bạn sử dụng ít hơn một cách hiệu quả.
+
+         <br/>4. <i>Đường</i> – Trong một số nghiên cứu hạn chế, người ta đã chứng minh rằng việc thêm đường vào đất có thể giúp giảm lượng nitơ trong đất của bạn. Đường có một phần bao gồm carbon, một nguyên tố thu hút và hấp thụ nitơ trong đất. Khái niệm này tương tự như việc bổ sung mùn cưa/dăm gỗ có hàm lượng carbon cao.
+
+         <br/>5. Thêm phân đã ủ vào đất.
+
+         <br/>6. Trồng các loại cây cố định đạm như đậu Hà Lan hoặc các loại đậu.
+
+         <br/>7. <i>Sử dụng phân NPK có hàm lượng N cao.
+
+         <br/>8. <i>Không làm gì</i> – Điều này có vẻ phản trực giác, nhưng nếu bạn đã có những cây ra nhiều tán lá, tốt nhất bạn nên để chúng tiếp tục hấp thụ toàn bộ nitơ để cải tạo đất cho lần tiếp theo của bạn. cây trồng.</div>""",
+       
+       'PHigh': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị P trong đất của bạn cao.</h1></div>
+         <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/> Hãy xem xét những gợi ý sau:
+
+         <br/><br/>1. <i>Tránh bón phân</i> – phân chứa nhiều chất dinh dưỡng quan trọng cho đất nhưng thường chứa hàm lượng phốt pho cao. Hạn chế bón thêm phân chuồng sẽ giúp giảm lượng lân được bổ sung.
+
+         <br/>2. <i>Chỉ sử dụng phân bón không chứa phốt pho</i> – nếu bạn có thể hạn chế lượng phốt pho thêm vào đất, bạn có thể để cây sử dụng lượng phốt pho hiện có trong khi vẫn cung cấp các chất dinh dưỡng quan trọng khác như Nitơ và Kali. Tìm loại phân bón có số như 10-0-10, trong đó số 0 không đại diện cho phốt pho.
+
+         <br/>3. <i>Tưới nước cho đất</i> – ngâm đất thường xuyên sẽ giúp đẩy phốt pho ra khỏi đất. Đây được coi là nỗ lực cuối cùng.
+
+         <br/>4. Trồng các loại rau cố định đạm để tăng nitơ mà không tăng phốt pho (như các loại đậu và đậu Hà Lan).
+
+         <br/>5. Sử dụng luân canh cây trồng để giảm lượng phốt pho cao</div>""",
+
+     'Cày': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị P trong đất của bạn thấp.</h1></div>
+         <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/> Hãy xem xét những gợi ý sau:
+
+         <br/><br/>1. <i>Bột xương</i> – một nguồn tác dụng nhanh được làm từ xương động vật xay rất giàu phốt pho.
+
+         <br/>2. <i>Đá photphat</i> – nguồn tác dụng chậm hơn khi đất cần chuyển hóa photphat trong đá thành phốt pho mà cây trồng có thể sử dụng.
+
+         <br/>3. <i>Phân lân</i> – bón phân có hàm lượng phốt pho cao theo tỷ lệ NPK (ví dụ: 10-20-10, 20 là tỷ lệ phốt pho).
+
+         <br/>4. <i>Phân hữu cơ</i> – thêm phân hữu cơ chất lượng vào đất của bạn sẽ giúp tăng hàm lượng phốt pho.
+
+         <br/>5. <i>Phân bón</i> – cũng như phân trộn, phân bón có thể là nguồn cung cấp phốt pho tuyệt vời cho cây trồng của bạn.
+
+         <br/>6. <i>Đất sét</i> – đưa các hạt đất sét vào đất của bạn có thể giúp giữ lại và khắc phục tình trạng thiếu hụt phốt pho.
+
+         <br/>7. <i>Đảm bảo độ pH của đất thích hợp</i> – độ pH trong khoảng 6,0 đến 7,0 đã được khoa học chứng minh là có khả năng hấp thu phốt pho tối ưu ở thực vật.
+
+         <br/>8. Nếu độ pH của đất thấp, hãy thêm vôi hoặc kali cacbonat vào đất làm phân bón. Canxi cacbonat tinh khiết rất hiệu quả trong việc tăng giá trị pH của đất.
+
+         <br/>6. style="color:#2c323f;font-weight: bold;" Nếu độ pH cao, việc bổ sung một lượng chất hữu cơ đáng kể sẽ giúp đất bị axit hóa. Việc sử dụng các loại phân bón có tính axit hóa, chẳng hạn như amoni sunfat, có thể giúp giảm độ pH của đất</div>""",
+
+       'KHigh': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị K của đất của bạn cao</h1></div></b>.
+         <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/> Hãy xem xét những gợi ý sau:
+
+         <br/><br/>1. <i>Xới đất</i> sâu bằng xẻng và tưới nước thật kỹ để hòa tan kali tan trong nước. Để đất khô hoàn toàn rồi lặp lại việc đào và tưới nước thêm hai hoặc ba lần nữa.
+
+         <br/>2. <i>Sàng lọc đất</i> và loại bỏ càng nhiều đá càng tốt bằng máy sàng đất. Các khoáng chất có trong đá như mica và fenspat giải phóng chậm kali vào đất qua quá trình phong hóa.
+
+         <br/>3. Ngừng bón phân thương mại giàu kali. Chỉ bón phân thương mại có số '0' ở trường số cuối cùng. Phân bón thương mại sử dụng hệ thống ba con số để đo hàm lượng nitơ, phốt pho và kali. Số cuối cùng là viết tắt của kali. Một lựa chọn khác là ngừng sử dụng tất cả các loại phân bón thương mại và bắt đầu chỉ sử dụng chất hữu cơ để làm giàu đất.
+
+         <br/>4. Trộn vỏ trứng nghiền, vỏ sò nghiền, tro gỗ hoặc đá mềm lân vào đất để bổ sung canxi. Trộn tối đa 10% phân hữu cơ để giúp cải tạo và cân bằng đất.
+
+         <br/>5. Sử dụng phân NPK có hàm lượng K thấp và phân hữu cơ vì chúng có giá trị NPK thấp.
+
+         <br/>6. Trồng một loại cây họ đậu che phủ để cố định đạm trong đất. Cách làm này sẽ đáp ứng nhu cầu nitơ của đất mà không làm tăng lượng phốt pho hoặc kali.
+         </div>""",
+
+         'Klow': """ <h1 style="font-size: 3.2rem; color: #203856; font-family: UVF;" class="jumbotron-heading; pb-5"> Giá trị K trong đất của bạn thấp.</h1></div>
+          <div class="ml-3 col-md-12 text-left" style="color:#2c323f;font-weight: bold;"><br/>Hãy xem xét những gợi ý sau:
+
+         <br/><br/>1. Trộn trong muricate của kali hoặc sunfat của kali
+         <br/>2. Hãy thử bột tảo bẹ hoặc rong biển
+         <br/>3. Hãy thử Sul-Po-Mag
+         <br/>4. Chôn vỏ chuối cách bề mặt đất một inch
+         <br/>5. Sử dụng phân kali vì chúng chứa kali có giá trị cao
+         </div>
+         """
+    }
